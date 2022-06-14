a01ce61e30dc7df1b00e8d7ea105b272d946c76c
Genvondoom <80456434+Genvondoom@users.noreply.github.com>
2022-06-14 07:32:53 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/firebasedoctutorial/MainActivity.kt
+++ app/src/main/java/com/example/firebasedoctutorial/MainActivity.kt
@@ -2,13 +2,43 @@ package com.example.firebasedoctutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.FirebaseApp
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var name: EditText
+    lateinit var email: EditText
+    lateinit var submit: Button
+    lateinit var listView:ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        name = findViewById(R.id.name)
+        email = findViewById(R.id.email)
+        submit = findViewById(R.id.submit)
+        listView = findViewById(R.id.entries)
+        val adapter: ArrayAdapter<*>
+        val users = mutableListOf<String>()
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,users)
+        listView.adapter = adapter
+        val database = FirebaseDatabase.getInstance().reference
+        //val test2 = mapOf("tester" to User("vd", "email"))
+        //database.child("test/hello").setValue(test2)
+
+        database.child("test/hello").get().addOnSuccessListener{
+
+            val test = it.value
+            println()
+            users.add(test.toString())
+            adapter.notifyDataSetChanged()
+        }
+
+
 
     }
 }
